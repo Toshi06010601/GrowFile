d77427463b70e701376ed402b180a21a6688f3fe
Toshinobu Nakamura <toshi@Mac.cust.communityfibre.co.uk>
2025-09-28 09:37:48 +0100

    Added studyrecords model

---
+++ growfile/database/migrations/2025_09_27_112221_create_study_records_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('study_records', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('title');
+            $table->text('content');
+            $table->date('studied_on');
+            $table->integer('duration_minutes');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('study_records');
+    }
+};
