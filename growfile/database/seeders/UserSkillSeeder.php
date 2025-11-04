d19926cd97ba4fb97e768e89c619a622bd941bd1
Toshinobu Nakamura <nkmr104@outlook.com>
2025-11-04 09:02:44 +0000

    Add: Skill and UserSkill Models, migration files, seeders

---
+++ growfile/database/seeders/UserSkillSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\UserSkill;
+
+class UserSkillSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        UserSkill::create([
+            'user_id' => 1,
+            'skill_id' => 1,
+            'level' => 5,
+        ]);
+    }
+}
