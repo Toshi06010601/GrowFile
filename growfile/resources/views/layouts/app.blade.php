de248e3056bf62d72a561948f4e344b7ea375584
Toshinobu Nakamura <nkmr104@outlook.com>
2025-11-21 08:48:34 +0000

    Add: Navigation section for guests

--- growfile/resources/views/layouts/app.blade.php
+++ growfile/resources/views/layouts/app.blade.php
@@ -16,7 +16,11 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+            @auth
+                @include('layouts.navigation')
+            @else
+                @include('layouts.navigation-for-guest')
+            @endauth
 
             <!-- Page Content -->
             <main>
