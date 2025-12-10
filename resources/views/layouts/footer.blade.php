ae8cca85fa8348f8f8f3f25a1d712e5ca531bc07
Toshinobu Nakamura <nkmr104@outlook.com>
2025-12-10 23:02:17 +0000

    Refactor: Fix appearance issue in mobile mode

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="sm:min-h-56 flex flex-col justify-center bg-white border-b border-gray-100 pl-10 pr-0 sm:px-40">
+<footer class="sm:min-h-56 flex flex-col justify-center bg-white border-b border-gray-100 px-10 sm:px-40">
     <div class="flex flex-row justify-around gap-10 items-start pt-5 sm:pt-8 pb-8">
         <figure class="flex-1 flex flex-col max-w-96">
             <!-- Logo -->
@@ -12,49 +12,46 @@
                 like-minded engineers
             </figcaption>
         </figure>
-        @if (Route::is('home'))
-            <div class="flex flex-col justify-start gap-3 sm:flex-row sm:justify-around ">
+        <div class="flex flex-col sm:flex-row sm:justify-around justify-start gap-3 sm:gap-10">
+                @if (Route::is('home'))
+                    <nav class="flex flex-col gap-1 max-w-72 items-start">
+                        <h3 class="text-lg  sm:text-2xl font-extrabold">Page map</h3>
+
+                        <div class="ml-1 text-sm sm:text-lg font-light text-gray-600 flex flex-col gap-1 items-start">
+                            <a href="#features" class="hover:text-black">
+                                <p>Features</p>
+                            </a>
+
+                            <a href="#how-to-use" class="hover:text-black">
+                                <p>How to use</p>
+                            </a>
+                        </div>
+                    </nav>
+                @endif
                 <nav class="flex flex-col gap-1 max-w-72 items-start">
-                    <h3 class="text-lg  sm:text-2xl font-extrabold">Page map</h3>
-
+                    <h3 class="text-lg sm:text-2xl font-extrabold">Site map</h3>
+    
                     <div class="ml-1 text-sm sm:text-lg font-light text-gray-600 flex flex-col gap-1 items-start">
-                        <a href="#features" class="hover:text-black">
-                            <p>Features</p>
+                        <a href="{{ route('home') }}" class="hover:text-black">
+                            <p>Home</p>
                         </a>
-
-                        <a href="#how-to-use" class="hover:text-black">
-                            <p>How to use</p>
+    
+                        <a href="{{ route('professional_profile.index') }}" class="hover:text-black">
+                            <p>Network</p>
+                        </a>
+    
+                        @auth
+                            <a class="flex flex-col justify-end items-center hover:text-black"
+                                href={{ route('professional_profile.show', $userProfile->slug) }}>
+                                <p>Profile</p>
+                            </a>
+                        @endauth
+    
+                        <a href="{{ route('profile.edit') }}" class="hover:text-black">
+                            <p>Account</p>
                         </a>
                     </div>
                 </nav>
-
             </div>
-        @endif
-        <div class="flex flex-col gap-3 sm:flex-row sm:justify-around ">
-            <nav class="flex flex-col gap-1 max-w-72 items-start">
-                <h3 class="text-lg sm:text-2xl font-extrabold">Site map</h3>
-
-                <div class="ml-1 text-sm sm:text-lg font-light text-gray-600 flex flex-col gap-1 items-start">
-                    <a href="{{ route('home') }}" class="hover:text-black">
-                        <p>Home</p>
-                    </a>
-
-                    <a href="{{ route('professional_profile.index') }}" class="hover:text-black">
-                        <p>Network</p>
-                    </a>
-
-                    @auth
-                        <a class="flex flex-col justify-end items-center hover:text-black"
-                            href={{ route('professional_profile.show', $userProfile->slug) }}>
-                            <p>Profile</p>
-                        </a>
-                    @endauth
-
-                    <a href="{{ route('profile.edit') }}" class="hover:text-black">
-                        <p>Account</p>
-                    </a>
-                </div>
-            </nav>
-        </div>
     </div>
 </footer>
