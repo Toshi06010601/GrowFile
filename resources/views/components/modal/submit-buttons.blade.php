4888d400c9f61787b8dcc6839c9a9f6a0287e55a
Toshinobu Nakamura <nkmr104@outlook.com>
2025-12-26 20:39:16 +0000

    Fix: Modify to prevent deletion upon Enter

--- resources/views/components/modal/submit-buttons.blade.php
+++ resources/views/components/modal/submit-buttons.blade.php
@@ -1,10 +1,17 @@
 @props(['name', 'deletable' => true])
 
 <div class="flex flex-row justify-end mt-6 gap-1">
-    <x-danger-button wire:click.prevent="delete" :class="$name == 'save' || !$deletable ? 'hidden' : ''">
-        {{ __('Delete') }}
-    </x-danger-button>
-    <x-primary-button>
+    @if($name !== 'save' && $deletable)
+        <x-danger-button 
+            type="button" {{--  Change type to button to stop Enter key submission --}}
+            wire:click.prevent="delete"
+            wire:confirm="Are you sure you want to delete this record?" {{-- Optional safety --}}
+        >
+            {{ __('Delete') }}
+        </x-danger-button>
+    @endif
+
+    <x-primary-button type="submit">
         {{ __($name) }}
     </x-primary-button>
 </div>
