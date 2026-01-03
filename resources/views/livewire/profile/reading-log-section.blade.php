506372e873d3b3ecf7c707b53a8a650dae319562
Toshinobu Nakamura <nkmr104@outlook.com>
2026-01-03 15:00:17 +0000

    Tweak: Incorporate progress bar for reading log

--- resources/views/livewire/profile/reading-log-section.blade.php
+++ resources/views/livewire/profile/reading-log-section.blade.php
@@ -19,7 +19,13 @@
     <ul class="flex flex-row max-w-full overflow-x-auto gap-4 snap-x snap-mandatory">
         @foreach ($readingLogs as $log)
             <li wire:key="{{ $log->id }}"
-                class="flex-none w-24 sm:w-32 snap-start flex flex-col p-3 gap-1 border shadow-md rounded-md bg-white">
+                class="flex-none w-24 sm:w-32 snap-start flex flex-col p-3 pb-5 gap-1 border shadow-md rounded-md bg-white relative">
+                {{-- Show Edit icon for the owner --}}
+                @if ($isOwner)
+                    <div class="absolute bottom-1 right-1 flex justify-end mt-2 min-w-5">
+                        <x-section.edit-icon x-on:click="$dispatch('set-reading-log', { id: {{ $log->id }} })" />
+                    </div>
+                @endif
 
                 <div class="flex flex-col gap-2">
                     {{-- Maintain aspect ratio so images don't stretch --}}
@@ -27,20 +33,21 @@
                         <img src="{{ $log->cover_url }}" alt="book_cover" class="w-full h-full object-cover">
                     </div>
 
-                    <div class="min-h-[3rem] flex flex-col justify-between">
-                        <p class="text-center text-sm sm:text-base font-medium line-clamp-2">
-                            {{ $log->status }}
-                        </p>
-
-                        {{-- Show Edit icon for the owner --}}
-                        @if ($isOwner)
-                            <div class="flex justify-center mt-2">
-                                <x-section.edit-icon
-                                    x-on:click="$dispatch('set-reading-log', { id: {{ $log->id }} })" />
+                    <div class="flex flex-col items-center gap-1">
+                        <div
+                            class="flex items-center w-full bg-gray-200 mt-2 rounded-full h-2.5 dark:bg-brand-secondary-200 overflow-hidden">
+                            <div class="bg-brand-primary-800 h-2.5 rounded-full transition-all duration-500"
+                                style="width: {{ ($log->current_page / $log->total_pages) * 100 }}%">
                             </div>
-                        @endif
+                        </div>
+                        <div class="flex items-center">
+                            <span class="text-xs text-gray-500 text-center">
+                                {{ round(($log->current_page / $log->total_pages) * 100) }}% complete
+                            </span>
+                        </div>
                     </div>
                 </div>
+
             </li>
         @endforeach
     </ul>
