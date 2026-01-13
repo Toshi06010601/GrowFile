39096f3e6b9968f6615315e7131a06af14274046
Toshinobu Nakamura <nkmr104@outlook.com>
2026-01-13 08:51:52 +0000

    Modify: Add disabled attribute for text input field

--- resources/views/components/modal/input-text.blade.php
+++ resources/views/components/modal/input-text.blade.php
@@ -1,9 +1,9 @@
-@props(['label', 'id', 'name', 'placeholder'])
+@props(['label', 'id', 'name', 'placeholder', 'disabled' => false])
 
 <div>
     <x-input-label :for="$id" :value="$label" class="text-lg mt-4" />
     <x-text-input :id="$id" type="text" class="mt-1 block w-full " :placeholder="$placeholder"
-        wire:model="{{ $name }}" />
+        wire:model="{{ $name }}" :disabled="$disabled" />
     <div>
         @error($name)
             <x-input-error :messages="$message" class="mt-2" />
