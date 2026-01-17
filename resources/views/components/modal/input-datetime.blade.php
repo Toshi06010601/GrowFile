af69646088a19ee4d80cce224724244e113e0003
Toshinobu Nakamura <nkmr104@outlook.com>
2026-01-17 10:47:32 +0000

    Refactor: Add disable input parameter

--- resources/views/components/modal/input-datetime.blade.php
+++ resources/views/components/modal/input-datetime.blade.php
@@ -1,8 +1,8 @@
-@props(['label', 'id', 'name'])
+@props(['label', 'id', 'name', 'disabled' => false])
 
 <div>
     <x-input-label :for="$id" :value="$label" class="text-lg mt-4" />
-    <x-text-input :id="$id" type="datetime-local" class="mt-1" wire:model="{{ $name }}" />
+    <x-text-input :id="$id" type="datetime-local" class="mt-1" wire:model="{{ $name }}" :disabled="$disabled" />
     <div>
         @error($name)
             <x-input-error :messages="$message" class="mt-2" />
