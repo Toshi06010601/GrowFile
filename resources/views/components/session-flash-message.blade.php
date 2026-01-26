961577ecc40762c8392f8d92ce615d2752747448
Toshinobu Nakamura <nkmr104@outlook.com>
2026-01-26 23:39:17 +0000

    Modify: Change bg-color

--- resources/views/components/session-flash-message.blade.php
+++ resources/views/components/session-flash-message.blade.php
@@ -6,7 +6,7 @@
         x-transition:enter="transition ease-out duration-1000" x-transition:enter-start="opacity-0 -translate-y-4"
         x-transition:enter-end="opacity-100 translate-y-0" x-transition:leave="transition ease-in duration-300"
         x-transition:leave-start="opacity-100 translate-y-0" x-transition:leave-end="opacity-0 translate-y-4"
-        class="fixed top-2 left-1/2 -translate-x-1/2 z-50 {{ session()->has('success') ? 'bg-brand-primary-900' : 'bg-red-900' }} text-white px-6 py-4 rounded-lg shadow-xl flex items-center gap-4 max-w-md">
+        class="fixed top-2 left-1/2 -translate-x-1/2 z-50 {{ session()->has('success') ? 'bg-brand-primary-900' : 'bg-red-600' }} text-white px-6 py-4 rounded-lg shadow-xl flex items-center gap-4 max-w-md">
 
         @if (session()->has('success'))
             <img src={{ asset('images/icons/check.svg') }} alt="check icon" class="size-6">
