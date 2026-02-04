4bad8a8a253ffafb7ee18c318481b26fc4ca52a5
Toshinobu Nakamura <nkmr104@outlook.com>
2026-02-04 21:13:49 +0000

    Refactor: Change from Article form to article editor to declutter

---
+++ app/Livewire/Profile/ArticleEditor.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Livewire\Profile;
+
+use Livewire\Component;
+use App\Livewire\Profile\ArticleSection;
+use Livewire\Attributes\On;
+use Livewire\Attributes\Locked;
+use App\Livewire\Forms\ArticleForm;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Article;
+use Livewire\WithFileUploads;
+use Exception;
+
+class ArticleEditor extends Component
+{
+    use WithFileUploads;
+
+    public ArticleForm $form;
+
+    #[Locked]
+    public $isOwner = false;
+
+    /*
+    Public functions for the modal form
+    */
+    #[On('set-article')]
+    public function setArticle($id)
+    {
+        $this->form->reset();
+
+        if($id) {
+            $article = Article::findOrFail($id);
+            $this->isOwner = Auth::id() === $article->user_id;
+            $this->form->setFields($article);
+        } else {
+            $this->isOwner = Auth::check();
+        }
+
+        $this->dispatch('open-modal', 'edit-article');
+    }
+
+    public function save()
+    {
+        $isUpdate = (bool) $this->form->article;
+
+        try {
+            $isUpdate && $this->authorize('update', $this->form->article);
+            $isUpdate ? $this->form->update() : $this->form->store();
+            $this->finishAction($isUpdate ? 'updated' : 'created');
+
+        } catch (Exception $e) {
+            $this->handleError($isUpdate ? 'update' : 'create', $e);
+        }
+    }
+    
+    public function delete()
+    {
+        try {
+            $this->authorize('delete', $this->form->article);
+            $this->form->article->delete();
+            $this->finishAction('deleted');
+    
+        } catch (Exception $e) {
+            $this->handleError('delete', $e);
+        }   
+    }
+
+    /*
+    Private functions for the modal form
+    */
+    private function finishAction(string $actionName): void
+    {
+        $this->dispatch('load-articles', 
+            type: 'success', 
+            message: "Article {$actionName} successfully."
+        )->to(ArticleSection::class);
+        $this->form->reset();
+        $this->dispatch('close-modal', 'edit-article');
+    }
+
+    private function handleError(string $actionName, Exception $e): void
+    {
+        $this->form->reset('article');
+        session()->flash('error', "Failed to {$actionName} article. Please try again.");
+        logger()->error("Article {$actionName} action failed.", ['error' => $e->getMessage()]);
+    }
+
+    public function render()
+    {
+        return view('livewire.profile.article-editor');
+    }
+}
