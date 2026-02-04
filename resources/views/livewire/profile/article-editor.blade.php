9402565535f29234aaf627e5b998f626f8ce29ea
Toshinobu Nakamura <nkmr104@outlook.com>
2026-02-04 21:17:03 +0000

    Refactor: Change blade file name and refactor

--- resources/views/livewire/profile/article-editor.blade.php
+++ resources/views/livewire/profile/article-editor.blade.php
@@ -5,48 +5,48 @@
     <x-modal.icon-close />
 
     {{-- form to update article --}}
-    <form wire:submit="{{ $article ? 'update' : 'save' }}" class="px-6 pt-14 pb-6">
+    <form wire:submit="save" class="px-6 pt-14 pb-6">
 
         {{-- Form title --}}
         <x-modal.header-title>
-            {{ $article ? 'Edit' : 'Add' }} Article
+            {{ $form->article ? 'Edit' : 'Add' }} Article
         </x-modal.header-title>
 
         {{-- Title --}}
-        <x-modal.input-text label="Name" id="title" name="title" placeholder="Name of your article"
+        <x-modal.input-text label="Title" id="title" name="form.title" placeholder="Name of your article"
             :disabled="!$isOwner" />
 
         {{-- Description --}}
-        <x-modal.input-textarea label="Description" id="description" name="description"
+        <x-modal.input-textarea label="Description" id="description" name="form.description"
             placeholder="Describe your article..." :disabled="!$isOwner" />
 
         {{-- Article URL --}}
-        <x-modal.input-text label="Article URL" id="article_url" name="article_url" placeholder="Article URL" :disabled="!$isOwner" />
+        <x-modal.input-text label="Article URL" id="article_url" name="form.article_url" placeholder="Article URL" :disabled="!$isOwner" />
 
-        <x-input-label for="article_image" value="article Image Path" class="text-lg mt-4" />
+        <x-input-label for="article_image" value="Article Image" class="text-lg mt-4" />
 
         {{-- article image path --}}
         <div>
             <div
                 class="aspect-video mt-1 w-full rounded-lg overflow-hidden mx-auto mb-4 border-2 border-brand-secondary-400">
                 {{-- Check if a new file has been selected --}}
-                @if ($article_image && Str::startsWith($article_image->getMimeType(), 'image/'))
-                    <img src="{{ $article_image->temporaryUrl() }}" alt="New article image preview"
+                @if ($form->article_image && Str::startsWith($form->article_image->getMimeType(), 'image/'))
+                    <img src="{{ $form->article_image->temporaryUrl() }}" alt="New article image preview"
                         class="w-full h-full object-cover">
                 @else
                     {{-- Fallback to the existing article image URL --}}
-                    <img src="{{ $article_image_path !== '' ? $article_image_path : '/storage/article_photos/default.jpg' }}"
+                    <img src="{{ $form->article_image_path !== '' ? asset("storage/{$form->article_image_path}") : '/storage/article_photos/default.jpg' }}"
                         alt="Current article image" class="w-full h-full object-cover">
                 @endif
             </div>
 
             @if ($isOwner)
                 {{-- File input field --}}
-                <input type="file" class="w-full" wire:model="article_image">
+                <input type="file" class="w-full" wire:model="form.article_image">
 
                 {{-- Display validation error --}}
                 <div>
-                    @error('article_image')
+                    @error('form.article_image')
                         <x-input-error :messages="$message" class="mt-2" />
                     @enderror
                 </div>
@@ -54,16 +54,16 @@
         </div>
 
         {{-- Platform Name --}}
-        <x-modal.input-text label="Platform Name" id="platform_name" name="platform_name" placeholder="Platform Name"
+        <x-modal.input-text label="Platform Name" id="platform_name" name="form.platform_name" placeholder="Platform Name"
             :disabled="!$isOwner" />
 
         {{-- Published Date --}}
-        <x-modal.input-date label="Published Date" id="published_date" name="published_date" placeholder="Describe your article..."
+        <x-modal.input-date label="Published Date" id="published_date" name="form.published_date" placeholder="Describe your article..."
             :disabled="!$isOwner" />
 
         @if ($isOwner)
             {{-- Save/Update button --}}
-            <x-modal.submit-buttons :name="$article ? 'update' : 'save'" />
+            <x-modal.submit-buttons :name="$form->article ? 'update' : 'save'" />
         @endif
     </form>
 
