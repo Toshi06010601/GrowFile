17ea661bf8e7259cc1a7c912270c679ff31f6552
Toshinobu Nakamura <nkmr104@outlook.com>
2026-02-13 08:38:26 +0000

    Refactor: create BackgroundForm and BackgroundEditor components for managing background images

---
+++ app/Livewire/Forms/BackgroundForm.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Livewire\Forms;
+
+use App\Models\Profile;
+use Livewire\Attributes\Validate;
+use Livewire\Form;
+use Illuminate\Support\Facades\DB;
+use App\Traits\HasImageUpload;
+use Illuminate\Support\Facades\Auth;
+use Exception;
+
+class BackgroundForm extends Form
+{
+    use HasImageUpload;
+
+    public ?Profile $profile = null;
+
+    #[Validate('nullable|mimes:jpg,jpeg,png,webp|max:1024')]
+    public $background_image = null;       // Holds the temporary uploaded file object
+    
+    #[Validate('nullable|string')]
+    public $background_image_path = '';  
+
+    /*
+    Public functions for the modal form
+    */
+    public function setFields(Profile $profile)
+    {
+        $this->resetValidation();
+
+        $this->profile = $profile;
+ 
+        $this->fill([
+                ...$profile->only('background_image_path'),
+            ]);
+    }
+
+
+    public function update()
+    {
+        DB::transaction(function ()
+        {
+            // 1. Validate
+            $this->validate();
+
+            // throw new Exception("Error Processing Request", 1);
+            
+            // 2. Upload Image
+            $this->uploadBackgroundImage();
+
+            // 3. Update profile
+            $this->profile->update(
+               $this->only('background_image_path')
+            );
+     
+        });
+    }
+
+    /*
+    Protected functions for the modal form
+    */
+    protected function uploadBackgroundImage()
+    {
+        if (!$this->background_image) {
+            return;
+        }
+
+        $oldPath = $this->background_image_path;
+
+        // 1. Upload new image and store file path
+        $this->background_image_path = $this->uploadNewImage(
+            $this->background_image,
+            'background_photos',
+            $oldPath
+        );
+
+        // 2. Delete old image if successfull
+        DB::aftercommit(fn() => $this->deleteOldImage( $oldPath, '/default.jpg' ));
+    }
+
+}
--- resources/views/livewire/profile/background-editor.blade.php
+++ resources/views/livewire/profile/background-editor.blade.php
@@ -3,7 +3,7 @@
 
     <x-modal.icon-close />
 
-    <form wire:submit="update" class="px-6 pt-14 pb-6">
+    <form wire:submit="save" class="px-6 pt-14 pb-6">
 
         <x-modal.header-title>
             Edit Background
@@ -11,21 +11,21 @@
 
         <div class="max-h-32 rounded-lg overflow-hidden mx-auto mb-4 border-2 border-brand-secondary-400">
             {{-- Check if a new file has been selected --}}
-            @if ($background_image && Str::startsWith($background_image->getMimeType(), 'image/'))
-                <img src="{{ $background_image->temporaryUrl() }}" alt="New background image preview"
+            @if ($form->background_image && Str::startsWith($form->background_image->getMimeType(), 'image/'))
+                <img src="{{ $form->background_image->temporaryUrl() }}" alt="New background image preview"
                     class="w-full h-full object-cover">
             @else
                 {{-- Fallback to the existing background image URL --}}
-                <img src="{{ $background_image_path }}" alt="Current background image" class="w-full h-full object-cover">
+                <img src="{{ $form->background_image_path ? asset("/storage/{$form->background_image_path}") : '/storage/background_photos/default.jpg' }}" alt="Current background image" class="w-full h-full object-cover">
             @endif
         </div>
 
         {{-- File input field --}}
-        <input type="file" class="w-full" wire:model="background_image">
+        <input type="file" class="w-full" wire:model.blur="form.background_image">
 
         {{-- Display validation error --}}
         <div>
-            @error('background_image')
+            @error('form.background_image')
                 <x-input-error :messages="$message" class="mt-2" />
             @enderror
         </div>
