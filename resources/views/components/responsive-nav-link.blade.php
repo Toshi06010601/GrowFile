6ea1c753551bf1aeba29733f1e6fabb60836a809
Toshinobu Nakamura <nkmr104@outlook.com>
2026-02-22 08:37:07 +0000

    Refactor: update responsive navigation link component structure and props

--- resources/views/components/responsive-nav-link.blade.php
+++ resources/views/components/responsive-nav-link.blade.php
@@ -1,11 +1,9 @@
-@props(['active'])
+@props(['href' => null, 'isPcView' => false])
 
-@php
-$classes = ($active ?? false)
-            ? 'block w-full ps-3 pe-4 py-2 border-l-4 border-indigo-400 text-start text-base font-medium text-indigo-700 bg-indigo-50 focus:outline-none focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out';
-@endphp
-
-<a {{ $attributes->merge(['class' => $classes]) }}>
-    {{ $slot }}
-</a>
+<div
+    class="text-base leading-4 font-medium text-brand-secondary-500 hover:text-brand-secondary-800 focus:outline-none transition ease-in-out duration-150"
+    :class="{{ $isPcView && 'hidden md:block' }}">
+    <a {{ $attributes->merge(['class' => 'flex flex-col justify-end items-center hover:scale-105']) }} href="{{ $href }}" wire:navigate.hover>
+        {{ $slot }}
+    </a>
+</div>
