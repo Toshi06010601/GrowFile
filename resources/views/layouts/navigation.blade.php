461c04203395f22de0f7f219adf17f01a05c23ef
Toshinobu Nakamura <nkmr104@outlook.com>
2026-02-22 08:35:13 +0000

    Refactor: simplify navigation component structure and update profile link handling

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,8 +1,8 @@
 <nav x-data="{
     open: false,
     profile_image_path: '{{ $userProfile ? $userProfile->profile_image_path : '' }}',
-}" @set-profile-menu-icon.window="profile_image_path = event.detail[0].filePath;"
-    class="bg-brand-tertiary-50 border-b border-brand-secondary-100 py-2 pr-5 lg:pr-0">
+}" @set-profile-menu-icon.window="profile_image_path = event.detail.filePath;"
+    class="bg-brand-tertiary-50 border-b border-brand-secondary-100 py-2 md:pr-5 lg:pr-0">
     <!-- Primary Navigation Menu -->
     <div class="max-w-6xl mx-auto">
         <div class="flex justify-between h-16">
@@ -20,30 +20,19 @@
                 </div>
 
                 <!-- Network button -->
-                <div
-                    class="hidden md:block text-base leading-4 font-medium text-brand-secondary-500 hover:text-brand-secondary-800 hover:scale-105 focus:outline-none transition ease-in-out duration-150">
-                    <a class="flex flex-col justify-center items-center"
-                        href="{{ route('professional_profile.index') }}" wire:navigate.hover>
-                        <img src="{{ asset('/images/icons/people.svg') }}" alt=""
-                            class="block h-8 sm:h-9 w-auto" />
-                        <p>Network</p>
-                    </a>
-                </div>
+                <x-responsive-nav-link :href="route('professional_profile.index')" :isPcView="true">
+                    <img src="{{ asset('/images/icons/people.svg') }}" alt="Network" class="block h-9 w-auto mb-1" />
+                    <p>Network</p>
+                </x-responsive-nav-link>
 
                 {{-- For Logged in user --}}
                 @auth
                     <!-- Profile button -->
-                    <div
-                        class="hidden md:block text-base leading-4 font-medium text-brand-secondary-500 hover:text-brand-secondary-800 focus:outline-none transition ease-in-out duration-150">
-                        <a class="flex flex-col justify-end items-center hover:scale-105"
-                            href={{ route('professional_profile.show', $userProfile ? $userProfile->slug : '') }} wire:navigate.hover>
-                            <div class="size-9 rounded-full overflow-hidden">
-                                <img src="{{ asset('/images/icons/profile.svg') }}" alt=""
-                                    class="w-full h-full object-cover" />
-                            </div>
-                            <p>Profile</p>
-                        </a>
-                    </div>
+                    <x-responsive-nav-link :href="route('professional_profile.show', $userProfile ? $userProfile->slug : '')" :isPcView="true">
+                        <img src="{{ asset('/images/icons/profile.svg') }}" alt="profile"
+                            class="block h-9 w-auto mb-1" />
+                        <p>Profile</p>
+                    </x-responsive-nav-link>
 
                     <!-- Settings Dropdown -->
                     <div class="hidden md:flex md:items-center md:ms-6">
@@ -90,82 +79,21 @@
                         </x-dropdown>
                     </div>
 
-                {{-- For guest user --}}
+                    {{-- For guest user --}}
                 @else
                     <!-- Login menu -->
                     <div class="hidden lg:flex sm:items-center sm:ms-6 sm:gap-2">
-                        <x-primary-button :href="route('register')" wire:navigate.hover class="hover:scale-105">Start Now</x-primary-button>
-                        <x-secondary-button :href="route('login')" wire:navigate class="hover:scale-105">Login</x-secondary-button>
+                        <x-primary-button :href="route('register')" wire:navigate.hover class="hover:scale-105">Start
+                            Now</x-primary-button>
+                        <x-secondary-button :href="route('login')" wire:navigate
+                            class="hover:scale-105">Login</x-secondary-button>
                     </div>
                 @endauth
 
 
             </div>
-
-
-            <!-- Hamburger -->
-            <div class="-me-2 flex items-center md:hidden">
-                <button @click="open = ! open"
-                    class="inline-flex items-center justify-center p-2 rounded-md text-brand-secondary-400 hover:text-brand-secondary-500 hover:bg-brand-secondary-100 focus:outline-none focus:bg-brand-secondary-100 focus:text-brand-secondary-500 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
-                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
-                            stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
         </div>
     </div>
 
-    <!-- Responsive Navigation Menu -->
-    <div :class="{ 'block': open, 'hidden': !open }" class="block md:hidden">
-        <div class="pt-2 pb-3">
-            <x-responsive-nav-link :href="route('home')" wire:navigate.hover class="flex flex-row gap-2 hover:bg-brand-secondary-300">
-                <img src="{{ asset('/images/icons/house.svg') }}" alt="" class="block h-6 w-auto" />
-                {{ __('Home') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('professional_profile.index')" wire:navigate.hover class="flex flex-row gap-2">
-                <img src="{{ asset('/images/icons/people.svg') }}" alt="" class="block h-6 w-auto" />
-                {{ __('Network') }}
-            </x-responsive-nav-link>
-            {{-- For Logged in user only --}}
-            @auth
-                <x-responsive-nav-link :href="route('professional_profile.show', $userProfile ? $userProfile->slug : '')" wire:navigate.hover class="flex flex-row gap-2">
-                    <img src="{{ asset('/images/icons/profile.svg') }}" alt="" class="block h-6 w-auto" />
-                    {{ __('Profile') }}
-                </x-responsive-nav-link>
-            @endauth
-        </div>
 
-        <!-- Responsive Settings Options -->
-        <div class="pt-2 pb-1 border-t border-brand-secondary-200">
-            {{-- For login user --}}
-            @auth
-                <div class="mt-1">
-                    <x-responsive-nav-link :href="route('profile.edit')">
-                        {{ __('Account') }}
-                    </x-responsive-nav-link>
-
-                    <!-- Authentication -->
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-
-                        <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                            {{ __('Log Out') }}
-                        </x-responsive-nav-link>
-                    </form>
-                </div>
-            {{-- For guest user --}}
-            @else
-                <div class="mt-2 flex flex-row justify-center gap-3">
-                    <x-primary-button :href="route('register')" wire:navigate.hover>Start Now</x-primary-button>
-                    <x-secondary-button :href="route('login')" wire:navigate.hover>Login</x-secondary-button>
-                </div>
-            @endauth
-        </div>
-    </div>
 </nav>
