cca0a550fc5b641600f978e1ca9bedfa95260ea0
Toshinobu Nakamura <nkmr104@outlook.com>
2026-02-24 08:12:32 +0000

    Add SetLocale middleware to handle locale settings for requests

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
+
+class SetLocale
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $locale = $request->route('locale')
+            ?? auth()->user()?->locale
+            ?? session('locale')
+            ?? config('app.locale');
+
+        if (in_array($locale, config('app.supported_locales', ['en']))) {
+            App::setLocale($locale);
+            session(['locale' => $locale]);
+        }
+        
+        URL::defaults(['locale' => $locale ]);
+
+        return $next($request);
+    }
+}
