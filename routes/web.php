4d21f2d777d8519cf8d50d729237a99d300f4d16
Toshinobu Nakamura <nkmr104@outlook.com>
2026-02-24 08:09:14 +0000

    Refactor: add locale prefix to profile and professional profile routes

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,12 @@ use App\Http\Controllers\RegisterProfileController;
 use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::prefix('{locale}')->get('/', function () {
     return view('welcome');
 })->name('home');
 
 //Profile Routes
-Route::middleware('auth')->group(function () {
+Route::middleware('auth')->prefix('{locale}')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -20,17 +20,12 @@ Route::middleware('auth')->group(function () {
 
 
 //Professional Profile Routes
-Route::get('professional_profile/results', [ProfessionalProfileController::class, 'index']
-)->name('professional_profile.index');
-
-Route::get('professional_profile/create', [ProfessionalProfileController::class, 'create'])
-->name('professional_profile.create');
-
-Route::post('professional_profile', [ProfessionalProfileController::class, 'store'])
-->name('professional_profile.store');
-
-Route::get('professional_profile/{slug}', [ProfessionalProfileController::class, 'show']
-)->name('professional_profile.show');
+Route::controller(ProfessionalProfileController::class)->prefix('{locale}')->group(function () {
+    Route::get('/professional_profile/results', 'index')->name('professional_profile.index');
+    Route::get('/professional_profile/create', 'create')->name('professional_profile.create');
+    Route::post('/professional_profile', 'store')->name('professional_profile.store');
+    Route::get('/professional_profile/{slug}', 'show')->name('professional_profile.show');
+});
 
 
 //FullCalendar EventController Routes
