03023bf5cc23a5ffe9a5485cdae86e3ad84ada64
Toshinobu Nakamura <nkmr104@outlook.com>
2026-02-24 08:14:08 +0000

    Fix class binding for responsive navigation link component

--- resources/views/components/responsive-nav-link.blade.php
+++ resources/views/components/responsive-nav-link.blade.php
@@ -1,8 +1,7 @@
 @props(['href' => null, 'isPcView' => false])
 
 <div
-    class="text-base leading-4 font-medium text-brand-secondary-500 hover:text-brand-secondary-800 focus:outline-none transition ease-in-out duration-150"
-    :class="{{ $isPcView && 'hidden md:block' }}">
+    class="text-base leading-4 font-medium text-brand-secondary-500 hover:text-brand-secondary-800 focus:outline-none transition ease-in-out duration-150 {{ $isPcView ? 'hidden md:block' : '' }}">
     <a {{ $attributes->merge(['class' => 'flex flex-col justify-end items-center hover:scale-105']) }} href="{{ $href }}" wire:navigate.hover>
         {{ $slot }}
     </a>
