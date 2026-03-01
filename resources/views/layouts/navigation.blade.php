15e22dbfa6fe304377a0490ffb84bf3eb79f0ab8
Toshinobu Nakamura <nkmr104@outlook.com>
2026-03-01 10:16:07 +0000

    Refactor navigation component to improve localization and accessibility

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
     <!-- Primary Navigation Menu -->
     <div class="max-w-6xl mx-auto">
         <div class="flex justify-between h-16">
-            <div class="px-4 w-full flex items-center gap-4 md:gap-8">
+            <div class="px-4 w-full flex items-center gap-4 lg:gap-6">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('home') }}" wire:navigate.hover class="hover:scale-105">
@@ -20,33 +20,41 @@
                 </div>
 
                 <!-- Network button -->
-                <x-responsive-nav-link :href="route('professional_profile.index')" :isPcView="true">
-                    <img src="{{ asset('/images/icons/people.svg') }}" alt="Network" class="block h-9 w-auto mb-1" />
-                    <p>Network</p>
+                <x-responsive-nav-link :href="route('professional_profile.index')" :isPcView="true" class="hidden md:flex">
+                    @if (request()->routeIs('professional_profile.index'))
+                        <img src="{{ asset('/images/icons/network-selected.svg') }}" alt="profile" class="block h-8 w-auto mb-1" />
+                    @else
+                        <img src="{{ asset('/images/icons/network-unselected.svg') }}" alt="profile" class="block h-8 w-auto mb-1" />
+                    @endif
+                    <p>{{ __('navigation.network') }}</p>
                 </x-responsive-nav-link>
 
                 {{-- For Logged in user --}}
                 @auth
                     <!-- Profile button -->
-                    <x-responsive-nav-link :href="route('professional_profile.show', $userProfile ? $userProfile->slug : '')" :isPcView="true">
-                        <img src="{{ asset('/images/icons/profile.svg') }}" alt="profile"
-                            class="block h-9 w-auto mb-1" />
-                        <p>Profile</p>
+                    <x-responsive-nav-link :href="route('professional_profile.show', $userProfile ? $userProfile->slug : '')" :isPcView="true" class="hidden md:flex">
+                        @if (request()->routeIs('professional_profile.show'))
+                            <img src="{{ asset('/images/icons/edit-selected.svg') }}" alt="profile" class="block h-9 w-auto mb-1" />
+                        @else
+                            <img src="{{ asset('/images/icons/edit-unselected.svg') }}" alt="profile"
+                                class="block h-9 w-auto mb-1" />
+                        @endif
+                        <p>{{ __('navigation.record') }}</p>
                     </x-responsive-nav-link>
 
                     <!-- Settings Dropdown -->
-                    <div class="hidden md:flex md:items-center md:ms-6">
+                    <div class="hidden md:flex md:items-center">
                         <x-dropdown align="right" width="48">
                             <x-slot name="trigger">
                                 <button
-                                    class="flex flex-col justify-center pr-3 py-2 border border-transparent text-base leading-4 font-medium rounded-md text-brand-secondary-500 bg-secondary-background-50 hover:text-brand-secondary-800 hover:scale-105 focus:outline-none transition ease-in-out duration-150">
-                                    <div class="ml-1 size-9 sm:size-12 rounded-full overflow-hidden border-2">
+                                    class="flex flex-col justify-center items-center border border-transparent text-base leading-4 font-medium rounded-md text-brand-secondary-500 bg-secondary-background-50 hover:text-brand-secondary-800 hover:scale-105 focus:outline-none transition ease-in-out duration-150">
+                                    
+                                    <div class="h-9 w-9 rounded-full overflow-hidden border-2 mb-1">
                                         <img :src="'/storage/' + profile_image_path" alt="profile image"
                                             class="w-full h-full object-cover">
                                     </div>
                                     <div class="inline-flex justify-center items-center">
-                                        <div>Me</div>
-
+                                        <div>{{ __('navigation.me') }}</div>
                                         <div class="ms-1">
                                             <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
                                                 viewBox="0 0 20 20">
@@ -56,23 +64,22 @@
                                             </svg>
                                         </div>
                                     </div>
-
                                 </button>
                             </x-slot>
 
                             <x-slot name="content">
                                 <x-dropdown-link :href="route('profile.edit')">
-                                    {{ __('My Account') }}
+                                    {{ __('navigation.my-account') }}
                                 </x-dropdown-link>
 
                                 <!-- Authentication -->
                                 <form method="POST" action="{{ route('logout') }}">
                                     @csrf
 
-                                    <x-dropdown-link :href="route('logout')"
+                                    <x-dropdown-link
                                         onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                        {{ __('Log Out') }}
+                                        {{ __('navigation.logout') }}
                                     </x-dropdown-link>
                                 </form>
                             </x-slot>
@@ -83,10 +90,10 @@
                 @else
                     <!-- Login menu -->
                     <div class="hidden lg:flex sm:items-center sm:ms-6 sm:gap-2">
-                        <x-primary-button :href="route('register')" wire:navigate.hover class="hover:scale-105">Start
-                            Now</x-primary-button>
+                        <x-primary-button :href="route('register')" wire:navigate.hover
+                            class="hover:scale-105">{{ __('navigation.start-now') }}</x-primary-button>
                         <x-secondary-button :href="route('login')" wire:navigate
-                            class="hover:scale-105">Login</x-secondary-button>
+                            class="hover:scale-105">{{ __('navigation.login') }}</x-secondary-button>
                     </div>
                 @endauth
 
