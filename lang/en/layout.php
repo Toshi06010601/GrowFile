18c8f33848be87f3064d88096604573c1e830d14
Toshinobu Nakamura <nkmr104@outlook.com>
2026-03-01 09:58:30 +0000

    Add English and Japanese layout language files for localization support

---
+++ lang/en/layout.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Layout Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | English translations for layout components
+    |
+    */
+
+    'footer-description' => 'Let us help you showcase your learning ability to recruiters and expand your network with other like-minded engineers',
+    'page-map' => 'Page Map',
+    'features' => 'Features',
+    'how-to-use' => 'How to use',
+    'site-map' => 'Site Map',
+    'home' => 'Home',
+    'network' => 'Network',
+    'profile' => 'Profile',
+    'account' => 'Account',
+    'all-rights-reserved' => 'All rights reserved',
+    'me' => 'Me',
+    'my-account' => 'My Account',
+    'logout' => 'Log Out',
+    'start-now' => 'Start Now',
+    'login' => 'Login',
+];
