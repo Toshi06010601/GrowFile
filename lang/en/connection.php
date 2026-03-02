ccea6b9fdf5435b2dab57612e0ed3c7858a68a89
Toshinobu Nakamura <nkmr104@outlook.com>
2026-03-02 22:01:05 +0000

    Add connection status language lines for English and Japanese

---
+++ lang/en/connection.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Connection Status Language Lines (English)
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for connection status messages
+    |
+    */
+
+    'offline' => 'You are offline',
+    'back-online' => 'You are back online.',
+    'offline-error' => 'You are currently offline.',
+];
--- resources/views/components/connection-status-message.blade.php
+++ resources/views/components/connection-status-message.blade.php
@@ -1,7 +1,6 @@
 <div x-data="{ online: navigator.onLine }"
-    @online.window="online = true; $dispatch('flash-message', { type: 'success', message: 'You are back online.'});"
-    @offline.window="online = false; $dispatch('flash-message', { type: 'error', message: 'You are currently offline.'});">
+    @online.window="online = true; $dispatch('flash-message', { type: 'success', message: '{{ __('connection.back-online') }}' });"
+    @offline.window="online = false; $dispatch('flash-message', { type: 'error', message: '{{ __('connection.offline-error') }}' });">
     <div x-show="!online" x-cloak class="bg-red-600 text-white text-center p-2">
-        You are offline
+        {{ __('connection.offline') }}
     </div>
-</div>
