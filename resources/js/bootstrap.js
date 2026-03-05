b21bbef83a8749600470b88a6210fb614b9dcadf
Toshinobu Nakamura <nkmr104@outlook.com>
2026-03-05 07:57:20 +0000

    style: standardize import statements and formatting in bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,31 +1,29 @@
-import axios from 'axios';
-window.axios = axios;
+import axios from "axios"
+window.axios = axios
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest"
 
 //Import ChartJS and make it global object
-import Chart from 'chart.js/auto';
-window.Chart = Chart;
+import Chart from "chart.js/auto"
+window.Chart = Chart
 
 //Import FullCalendarJS and make it global object
-import { Calendar } from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction';
+import { Calendar } from "@fullcalendar/core"
+import dayGridPlugin from "@fullcalendar/daygrid"
+import timeGridPlugin from "@fullcalendar/timegrid"
+import interactionPlugin from "@fullcalendar/interaction"
 
 window.FullCalendar = {
     Calendar: Calendar,
     dayGridPlugin: dayGridPlugin,
     timeGridPlugin: timeGridPlugin,
-    interactionPlugin: interactionPlugin
-};
+    interactionPlugin: interactionPlugin,
+}
 
 //Import SweetAlertJS and make it global object
-import Swal from 'sweetalert2';
-window.Swal = Swal;
-
+import Swal from "sweetalert2"
+window.Swal = Swal
 
 // Import dropdownNavigator and make it global object
-import dropdownNavigator from './dropdown-navigator';
-window.dropdownNavigator = dropdownNavigator;
-
+import dropdownNavigator from "./dropdown-navigator"
+window.dropdownNavigator = dropdownNavigator
