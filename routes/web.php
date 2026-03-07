181e74071edaf04a3fee581a47a9e4d167ef18d0
Toshinobu Nakamura <nkmr104@outlook.com>
2026-03-07 08:28:58 +0000

    style: add throttling middleware to routes for improved rate limiting

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,17 @@ use App\Http\Controllers\ProfileSearchController;
 use App\Http\Controllers\ProfessionalProfileController;
 use App\Http\Controllers\RegisterProfileController;
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\LocaleController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\App;
 
-Route::prefix('{locale}')->get('/', function () {
+Route::prefix('{locale}')->middleware(['throttle:100,1'])->get('/', function () {
     return view('welcome');
 })->name('home');
 
 //Profile Routes
-Route::middleware(['auth', 'verified'])->prefix('{locale}')->group(function () {
+Route::middleware(['auth', 'verified', 'throttle:30,1'])->prefix('{locale}')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -23,15 +24,15 @@ Route::middleware(['auth', 'verified'])->prefix('{locale}')->group(function () {
 
 //Professional Profile Routes
 Route::controller(ProfessionalProfileController::class)->prefix('{locale}')->group(function () {
-    Route::get('/professional_profile/results', 'index')->name('professional_profile.index');
-    Route::middleware(['auth', 'verified'])->get('/professional_profile/create', 'create')->name('professional_profile.create');
-    Route::middleware(['auth', 'verified'])->post('/professional_profile', 'store')->name('professional_profile.store');
-    Route::get('/professional_profile/{slug}', 'show')->name('professional_profile.show');
+    Route::middleware(['throttle:100,1'])->get('/professional_profile/results', 'index')->name('professional_profile.index');
+    Route::middleware(['auth', 'verified', 'throttle:10,1'])->get('/professional_profile/create', 'create')->name('professional_profile.create');
+    Route::middleware(['auth', 'verified', 'throttle:10,1'])->post('/professional_profile', 'store')->name('professional_profile.store');
+    Route::middleware(['throttle:60,1'])->get('/professional_profile/{slug}', 'show')->name('professional_profile.show');
 });
 
 
 //FullCalendar EventController Routes
-Route::get('/api/events', [EventController::class, 'index']
+Route::middleware(['throttle:60,1'])->get('/api/events', [EventController::class, 'index']
 );
 
 // LocaleController Routes
